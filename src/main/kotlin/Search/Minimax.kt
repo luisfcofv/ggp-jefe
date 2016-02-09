3d24b86be85493930f841cbcee291c7ef77e6a87
Luis Flores <me@luisflores.mx>
2016-02-09 18:01:08 +0000

    Add alpha-beta pruning

--- src/main/kotlin/Search/Minimax.kt
+++ src/main/kotlin/Search/Minimax.kt
@@ -21,9 +21,8 @@ class Minimax : BaseSearch {
         var score = 0
         var bestMove = moves[0]
 
-
         for (move in moves) {
-            var result = minscore(stateMachineGamer.currentState, move)
+            var result = minscore(stateMachineGamer.currentState, move, 0, 100)
 
             if (result > score) {
                 score = result
@@ -35,12 +34,34 @@ class Minimax : BaseSearch {
         return MoveCandidate(bestMove, score)
     }
 
-    fun minscore(currentMachineState: MachineState, action: Move):Int {
+    fun maxscore(currentMachineState: MachineState, alpha: Int, beta: Int):Int {
+        if (stateMachineGamer.stateMachine.isTerminal(currentMachineState)) {
+            return stateMachineGamer.stateMachine.getGoal(currentMachineState, stateMachineGamer.role)
+        } else if (System.currentTimeMillis() > finishBy) {
+            return 1
+        }
+
+        var newAlpha = alpha
+        var moves = stateMachineGamer.stateMachine.getLegalMoves(currentMachineState, stateMachineGamer.role)
+
+        for (move in moves) {
+            var result = minscore(currentMachineState, move, alpha, beta)
+            newAlpha = Math.max(result, newAlpha)
+
+            if (newAlpha >= beta) {
+                return beta
+            }
+        }
+
+        return newAlpha
+    }
+
+    fun minscore(currentMachineState: MachineState, action: Move, alpha: Int, beta: Int):Int {
         if (System.currentTimeMillis() > finishBy) {
             return 1
         }
 
-        var score = 100
+        var newBeta = beta
         var opponentMoves = stateMachineGamer.stateMachine.getLegalMoves(currentMachineState, opponent)
 
         for (opponentMove in opponentMoves) {
@@ -48,34 +69,14 @@ class Minimax : BaseSearch {
             moves[roles!!.indexOf(opponent)] = opponentMove
 
             var newMachineState = stateMachineGamer.stateMachine.getNextState(currentMachineState, moves)
-            var result = maxscore(newMachineState)
+            var result = maxscore(newMachineState, alpha, beta)
+            newBeta = Math.min(newBeta, result)
 
-            if (result < score) {
-                score = result
+            if (beta <= alpha) {
+                return alpha
             }
         }
 
-        return score
-    }
-
-    fun maxscore(currentMachineState: MachineState):Int {
-        if (stateMachineGamer.stateMachine.isTerminal(currentMachineState)) {
-            return stateMachineGamer.stateMachine.getGoal(currentMachineState, stateMachineGamer.role)
-        } else if (System.currentTimeMillis() > finishBy) {
-            return 1
-        }
-
-        var score = 0
-        var moves = stateMachineGamer.stateMachine.getLegalMoves(currentMachineState, stateMachineGamer.role)
-
-        for (move in moves) {
-            var result = minscore(currentMachineState, move)
-
-            if (result > score) {
-                score = result
-            }
-        }
-
-        return score
+        return newBeta
     }
 }
